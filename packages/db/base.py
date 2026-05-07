adcfbe96cfc0c22b809831d4bcb9a5f0c20e31a2
kjw009 <traceropt@gmail.com>
2026-05-07 01:59:58 +0100

    chore: comments

--- packages/db/base.py
+++ packages/db/base.py
@@ -1,5 +1,15 @@
 from sqlalchemy.orm import DeclarativeBase
 
-
+# In SQLAlchemy 2.0+, inheriting from DeclarativeBase is the standard way 
+# to create a "Registry" for your database models.
 class Base(DeclarativeBase):
+    """
+    This is the foundational class for all database models.
+    
+    When you create a model (like 'User' or 'Product'), it will inherit from 
+    this 'Base'. SQLAlchemy uses this to:
+    1. Track all tables defined in your application.
+    2. Map Python classes to SQL tables.
+    3. Manage 'metadata' (the schema definition) used to create or drop tables.
+    """
     pass
