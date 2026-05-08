b721f3c3f6748892efae9128fcae723d2724095a
kjw009 <traceropt@gmail.com>
2026-05-08 04:44:34 +0100

    fixed: ruff errors

--- alembic/versions/4d68cffa046a_add_ebay_oauth_state_table.py
+++ alembic/versions/4d68cffa046a_add_ebay_oauth_state_table.py
@@ -5,17 +5,18 @@ Revises: a1b2c3d4e5f6
 Create Date: 2026-05-08 04:40:56.308107
 
 """
-from typing import Sequence, Union
+from collections.abc import Sequence
 
-from alembic import op
 import sqlalchemy as sa
 from sqlalchemy.dialects import postgresql
 
+from alembic import op
+
 # revision identifiers, used by Alembic.
 revision: str = '4d68cffa046a'
-down_revision: Union[str, None] = 'a1b2c3d4e5f6'
-branch_labels: Union[str, Sequence[str], None] = None
-depends_on: Union[str, Sequence[str], None] = None
+down_revision: str | None = 'a1b2c3d4e5f6'
+branch_labels: str | Sequence[str] | None = None
+depends_on: str | Sequence[str] | None = None
 
 
 def upgrade() -> None:
